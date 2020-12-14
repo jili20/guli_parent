e2eef1b7c32b77448040c0d8b81dcb058d3147cc
Bing <bing_yu2001@163.com>
2020-12-14 13:58:12 +0800

    canal 数据库同步

---
+++ canal_client/src/main/java/com/atguigu/canal/CanalApplication.java
@@ -0,0 +1,28 @@
+package com.atguigu.canal;
+
+import com.atguigu.canal.client.CanalClient;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.Resource;
+
+/**
+ * @author bing  @create 2020/12/14-11:59 上午
+ */
+@SpringBootApplication
+public class CanalApplication implements CommandLineRunner {
+    @Resource
+    private CanalClient canalClient;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        //项目启动，执行canal客户端监听
+        canalClient.run();
+    }
+}
+
